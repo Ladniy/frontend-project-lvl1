64e858b306c69af7a7d7d3a0cd6d47c35bb2da2e
Vladislav Morozov <ladniy101@gmail.com>
2022-01-17 23:13:19 +0800

    Little fix

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
   const username = readlineSync.question('May i have your name? ');
-  console.log(`Hi ${username}!`);
+  console.log(`Hi, ${username}`);
 };
 
 export default greeting;
