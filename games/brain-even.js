286ed69fec24c9624fafbb06a22b3e2f09018e60
Vladislav Morozov <ladniy101@gmail.com>
2022-01-17 23:52:54 +0800

    Rename func and variables

--- games/brain-even.js
+++ games/brain-even.js
@@ -2,23 +2,20 @@
 
 import gameEngine from '../src/index.js';
 
-const isEven = () => {
+const brainEven = () => {
   const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
   const questionAnswerPairs = [];
 
   for (let i = 0; i < 3; i += 1) {
-    const question = Math.round(Math.random() * 20);
-    const answer = question % 2 ? 'no' : 'yes';
-    const questionAnswerPair = [];
+    const randomNumber = Math.round(Math.random() * 20);
+    const rightAnswer = randomNumber % 2 ? 'no' : 'yes';
+    const bufferArray = [];
 
-    questionAnswerPair.push(question);
-    questionAnswerPair.push(answer);
-    questionAnswerPairs.push(questionAnswerPair);
-
-    // console.log(questionAnswerPairs);
-    // console.log(gameRules);
+    bufferArray.push(randomNumber);
+    bufferArray.push(rightAnswer);
+    questionAnswerPairs.push(bufferArray);
   }
   gameEngine(questionAnswerPairs, gameRules);
 };
 
-isEven();
+brainEven();
