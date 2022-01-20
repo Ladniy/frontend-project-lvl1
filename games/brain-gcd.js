fb1dc351aec950c10ee725d28e88238a1c819166
Vladislav Morozov <ladniy101@gmail.com>
2022-01-20 23:12:41 +0800

    Write basic function for finding gcd

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
 
-import gameEngine from '../src/index.js';
+// import gameEngine from '../src/index.js';
 
 // Functon to generate a random number
 const randomNumber = () => {
-  const result = Math.round(Math.random() * 30);
+  const result = Math.round(Math.random() * 50);
   return result;
 };
 
 const brainGcd = () => {
   const gameRules = 'Find the greatest common divisor of given numbers.';
+  const questionAnswerPairs = [];
 
-  gameEngine(questionAnswerPairs, gameRules);
+  let firstNumber = randomNumber();
+  let secondNumber = randomNumber();
+  let result = 0;
+  console.log(firstNumber);
+  console.log(secondNumber);
+  while (firstNumber !== secondNumber) {
+    if (firstNumber > secondNumber) {
+      firstNumber -= secondNumber;
+    } else {
+      secondNumber -= firstNumber;
+    }
+  }
+  result = firstNumber;
+  console.log(result);
+  // gameEngine(questionAnswerPairs, gameRules);
 };
 
 brainGcd();
