953ef24c32ffaf21f5c0f395ec8aa1647d5b9dd1
Ladniy <ladniy101@gmail.com>
2022-01-25 02:22:56 +0800

    Add createProgression and braiin-Progression function

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -7,4 +7,24 @@ const randomNumber = () => {
   return result;
 };
 
+const createProgression = (firstNumber, secondNumber) => {
+  const a = firstNumber;
+  let b = secondNumber;
+  let result = `${a}`;
+  console.log(result);
+  for (let i = 0; i < 9; i += 1) {
+    const bNumber = ` ${(b *= 2)}`;
+    result = result.concat(bNumber);
+  }
+  console.log(result);
+};
+
+const brainProgression = () => {
+  const a = randomNumber();
+  const b = Math.round(Math.random() * 4);
+  createProgression(a, b);
+};
+
+brainProgression();
+
 // gameEngine();
