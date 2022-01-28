b1257bc7f13587972c1c31a66682b389098cc71f
Vladislav Morozov <ladniy101@gmail.com>
2022-01-28 23:19:34 +0800

    Now function working properly

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -3,26 +3,31 @@
 // import gameEngine from "../src";
 
 const randomNumber = () => {
-  const result = Math.round(Math.random() * 2);
+  const result = Math.round(Math.random() * 6);
   return result;
 };
 
 const createProgression = (firstNumber, secondNumber) => {
+  const array = [];
   let a = firstNumber;
-  let b = secondNumber;
-  let result = `${a}`;
-  console.log(result);
+  const b = secondNumber;
+  // let result = `${a}`;
+  // console.log(result);
+  array.push(a);
   for (let i = 0; i < 9; i += 1) {
-    b += b;
-    const tempString = ` ${(a + b)}`;
-    result = result.concat(tempString);
+    a += b;
+    // const tempString = ` ${a}`;
+    // result = result.concat(tempString);
+    array.push(a);
   }
+  console.log(array);
+  const result = array.join(' ');
   console.log(result);
 };
 
 const brainProgression = () => {
-  const a = randomNumber();
-  const b = Math.round(Math.random() * 4);
+  const a = 2; // randomNumber();
+  const b = Math.round(Math.random() * 6);
   createProgression(a, b);
 };
 
