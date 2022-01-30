8234ada5ae438d94ffad68fe33938a5537608688
Vladislav Morozov <ladniy101@gmail.com>
2022-01-31 06:27:43 +0800

    Fix string

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-  const username = readlineSync.question('May i have your name? ');
+  const username = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${username}`);
 };
 
