c973c2fa3be14b9ae524a17e51f3e2ac9b3836f3
Vladislav Morozov <ladniy101@gmail.com>
2022-01-31 03:55:38 +0800

    Fix import, add some comments and move getRandomNumber function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-#!/usr/bin/env node
-
 import readlineSync from 'readline-sync';
 
-export const gameEngine = (array, rules) => {
+// Function for generate random nubmers, inclusive man and max integers
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Main game engine function
+const gameEngine = (array, rules) => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May i have your name? ');
   console.log(`Hello, ${username}!\n${rules}`);
+
   // Compresion and counting answers
   let rightAnswersCount = 0;
-
   for (let i = 0; i < 3; i += 1) {
     const userAnswer = readlineSync.question(`Question: ${array[i][0]}\nYour answer: `);
     if (array[i][1] === userAnswer) {
@@ -19,12 +23,11 @@ export const gameEngine = (array, rules) => {
       break;
     }
   }
-  // Message of game ending
+
+  // Checking answers count and show message of game ending
   if (rightAnswersCount === 3) {
     console.log(`Congratulations, ${username}`);
   }
 };
 
-export function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+export { gameEngine, getRandomNumber };
